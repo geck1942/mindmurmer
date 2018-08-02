7b0ec3909f3b4e37d020e49e77ef1b3ccd6ca9cb
Aurelien Jacquot <aurelien.jacquot@hotmail.fr>
2018-08-02 17:11:44 -0400

    added different meditation state flames to be shown

--- fr0st-master/fr0st/scripts/mindmurmer/default_eeg.py
+++ fr0st-master/fr0st/scripts/mindmurmer/default_eeg.py
@@ -37,6 +37,8 @@ class MMEngine:
         self.sinelength = 300 # frames
         self.gui = gui
         self.maxfps = 25 # target frames per second
+        self.states_flames = [ ] * 5
+        self.meditation_state = 0
 
         # init rabbitMQ connection
         self.rabbit = RabbitController('localhost', 5672, 'guest', 'guest', '/')
@@ -50,10 +52,9 @@ class MMEngine:
         # reference to global or defined herebefore
         self.flame = flame
 
-
     def start(self):
-        # reset counters
-        self.resetRendering()
+        # ask for input
+        self.show_input()
         # hide UI
         # self.gui.Hide()
         # show GUI preview Window
@@ -66,6 +67,7 @@ class MMEngine:
         # time.sleep(0.400)
         self.gui.previewframe.SetFocus()
         # start loop
+        self.flame = self.load_flame(self.states_flames[0])
         self.frame_index = 0
         self.keeprendering = True
         while self.keeprendering:
@@ -77,7 +79,7 @@ class MMEngine:
                 if(self.render() and self.gui.previewframe.rendering == False):
                     
                     # run pyflam4 rendering
-                    self.gui.previewframe.RenderPreview()
+                    self.gui.previewframe.RenderPreview(self.flame)
 
                     # about the latest eegdata:
                     eegdata = self.eeg_source.get_data()
@@ -85,15 +87,26 @@ class MMEngine:
                     # Retreive main color from flame
                     rgb = self.get_flamecolor_rgb()
                     color = wx.Colour(rgb[0], rgb[1], rgb[2], 1)
+                    eeg_meditation_state = int(round(len(self.states_flames) * eegdata.raw_meditatation_state(), 0))
 
                     #TODO get heartbeat
                     heartbeat = 60
                     
+                    # new meditation state reached
+                    if(self.meditation_state != eeg_meditation_state):
+                        print('NEW meditation state: %s using %s' %(eeg_meditation_state,
+                                                                    self.states_flames[eeg_meditation_state].name))
+                        # load flame from eegdata
+                        self.flame = self.load_flame(self.states_flames[eeg_meditation_state])
+                        # save
+                        self.meditation_state = eeg_meditation_state
+
                     # update status bar
-                    show_status("Frame: %s | Color: %s %s %s | EEG: %s" 
+                    show_status("Frame: %s | Color: %s %s %s | EEG: %s | MEDITATION STATE: %s" 
                                 %(self.frame_index, 
                                 color.red, color.green, color.blue,
-                                eegdata.console_string() if eegdata is not None else "" ))
+                                eegdata.console_string() if eegdata is not None else "",
+                                eeg_meditation_state ))
 
                     # send data to RabbitMQ bus
                     self.rabbit.publish_color(color)
@@ -163,13 +176,37 @@ class MMEngine:
         return [int(r / weight), int(g / weight), int(b / weight)]
     
 
-            
+    def show_input(self):
+        flames = get_flames()
+        res = dialog("Choose settings.\n\n(Keyframe interval = 0 "
+                 "uses the time attribute of each flame instead "
+                 "of a fixed interval)",
+                 ("MEDITATION STATE 0", flames, 0),
+                 ("MEDITATION STATE 1", flames, 1),
+                 ("MEDITATION STATE 2", flames, 2),
+                 ("MEDITATION STATE 3", flames, 3),
+                 ("MEDITATION STATE 4", flames, 4),
+                 ("MEDITATION STATE 5", flames, 5))
+        self.states_flames = res
+        if len(self.states_flames) < 2:
+            raise ValueError("Need to select at least 2 flames")
+        return
+
+    # lerp is interpolation percentage [0 - 1] between origin and target
+    def load_flame(self, flame_origin, flame_target = None, lerp = 0):
+        if(lerp == 0 or flame_target is None):
+            return flame_origin
+        if(lerp == 1 or flame_origin is None):
+            return flame_target
+
+        # interpolation:
+
     
         
     # process new EEGData and animate flame
     def render(self):
         docontinue = True
-        if (flame.xform is None or len(flame.xform) == 0):
+        if (self.flame.xform is None or len(self.flame.xform) == 0):
             return False
         try:
             # get eeg data as [] from ext. source
@@ -178,16 +215,16 @@ class MMEngine:
             # FLAME UPDATE (at least 125 frames apart)
             # if(eegdata.blink == 1 and self.frame_index > 125):
                 # adjust form weights (from utils)
-                #normalize_weights(flame)
+                #normalize_weights(self.flame)
                 
                 # move window
-                #flame.reframe()
+                #self.flame.reframe()
 
                 # update colors
-                #calculate_colors(flame.xform)
+                #calculate_colors(self.flame.xform)
 
             dataindex = 0
-            for x in flame.xform:
+            for x in self.flame.xform:
                 if(x.animate and eegdata is not None):
                     # ROTATION
                     # calculate rotation amount from data elements
@@ -211,101 +248,7 @@ class MMEngine:
             # SHOW preview on Fr0st
             return False
 
-    # Generate a whole new Flame
-    def NewFlame(self):
-        # if(not flame.final is None):
-        #     flame.final.delete()
-        # l = len(flame.xform)
-        # # cannot remove form #0
-        # for i in range(l-1):
-        #     flame.xform[-1].delete()
-        
-        # flame.xform[-1].delete()
-        #flame.xform =[]
-        
-        # based on julia lines:
-        # Form 1: BASE
-        if(len(flame.xform) == 0):
-            x1 = flame.add_xform()
-        else:
-            x1 = flame.xform[0]
-        # x1.coefs = [1, 0,    # x
-        #             0, 1,    # y
-        #             ran(-0.2, 0.2), ran(-0.2, 0.2) ]    # o around 0
-        x1.weight = 0.25
-        x1.color = random.random() # [0 : 1]
-        x1.color_speed = 0.9
-        x1.noise = 0.0 + random.random() * 0.4 # [0.0 : 0.4]
-        x1.blur = 0.0 + random.random() * 0.1 # [0.0 : 0.1]
-        x1.animate = 1
-        x1.rotate(random.random() * 360)
-
-        # Form 2: JULIA TRANSFORM
-        if(len(flame.xform) <= 1):
-            x2 = flame.add_xform()
-        else:
-            x2 = flame.xform[1]
-        # x2.coefs = [0.5, 0,    # x
-        #             0, 0.5,    # y
-        #             ran(-0.5, 0.5), ran(-0.5, 0.5) ]    # o
-        x2.weight = 0.5
-        # x2.color = random.random() # [0 : 1]
-        x2.color_speed = 0.5
-        x2.julian = 0.7 + random.random() * 0.3 # [0.7 : 1]
-        x2.animate = 1
-        x2.rotate(random.random() * 360)
-
-        # Form 3: LINE
-        if(len(flame.xform) <= 2):
-            x3 = flame.add_xform()
-        else:
-            x3 = flame.xform[2]
-        # x3.coefs = [ran(1, 3), 0,    # x
-        #             0, 0,    # y
-        #             ran(-1.0, 1.0), ran(-1.0, 1.0) ]    # o
-        x3.weight = 0.25
-        # x3.color = random.random() # [0 : 1]
-        x3.color_speed = 0.5
-        x3.cross = 0.5 + random.random() * 1.5 # [0.5 : 2]
-        x3.julian = 0.2 + random.random() * 0.8 # [0.2 : 1]
-        x3.animate = 1
-        x3.rotate(random.random() * 360)
-
-        # Form 4: FLOWERS
-        if(len(flame.xform) > 3):
-            x4 = flame.xform[3]
-            # x4.coefs = [0.5, 0,    # x
-            #             0, 0.5,    # y
-            #             ran(-0.5, 0.5), ran(-0.5, 0.5) ]    # o                    
-            x4.weight = 0.25
-            # x4.color = random.random() # [0 : 1]
-            x4.color_speed = 0.9
-            x4.julian = 0.9 + random.random() * 0.1 # [0.9 : 1]
-            x4.animate = 1
-            x4.rotate(random.random() * 360)
-
-        # Form 5: FINAL
-        if(not flame.final is None):
-            x5 = flame.final
-            # x5.coefs = [1, 0,    # x
-            #             0, 1,    # y
-            #             ran(-0.5, 0.5), ran(-0.5, 0.5) ]    # o around 0
-            # x5.color = 0.0
-            x5.color_speed = 0.0
-            x5.spherical = 0.1 + random.random() * 0.9 # [0.1 : 1]
-            x5.julia = 0.1 + random.random() * 0.4 # [0.1 : 0.5]
-            x5.linear = 0.0
-            x5.rotate(random.random() * 360)  
-
-
-    def resetRendering(self):
-        # from fr0stlib.gui.preview import PreviewFrame
-        # if(self.gui.previewframe.rendering):
-            # set the Preview as non-rendering
-            # self.gui.previewframe = PreviewFrame(self.gui.previewframe.parent)
-            # self.gui.previewframe.rendering = False
-        return
-
+    
 
 # RUN
 audio_folder = get_scriptpath() + "/mindmurmer/sounds_controllers/sound_controller_demo_files/soundscape_controller_demo_files"
