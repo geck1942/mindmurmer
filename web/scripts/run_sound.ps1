0b0f627f7185115e89f2285281a1f0ec672084c6
Aurelien Jacquot <aurelien.jacquot@hotmail.fr>
2018-08-14 23:38:45 -0700

    sound_controller powershell script

---
+++ web/scripts/run_sound.ps1
@@ -0,0 +1,4 @@
+$params = '--audio_folder fr0st-master/fr0st/scripts/mindmurmer/sounds_controller_files/16000hz-24bit/mp3/audio --up_transition_sound_filename fr0st-master/fr0st/scripts/mindmurmer/sounds_controller_files/16000hz-24bit/mp3/transition_up.mp3  --down_transition_sound_filename fr0st-master/fr0st/scripts/mindmurmer/sounds_controller_files/16000hz-24bit/mp3/transition_down.mp3 '
+py -2 "c:/Users/aurel/OneDrive/Documents/DEV ROOT/PYTHON/mindmurmer/fr0st-master/fr0st/scripts/mindmurmer/sound_controller.py" $params
+ 
+
